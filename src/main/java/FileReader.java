d057172b9d59bb18275bca6a43ef4a56acb05dd3
Phoenix <edinak26@gmail.com>
2019-09-25 00:12:23 +0300

    FileReader added class FileReader was created tests for FileReader were created files for reading were created

---
+++ src/main/java/FileReader.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class FileReader {
+    private static final String INPUT_FILE_PATH = "src\\main\\resources\\input.txt";
+    private static final String OUTPUT_FILE_PATH = "src\\main\\resources\\output.txt";
+
+    public static List<String> readInputFile(){
+        return read(INPUT_FILE_PATH);
+    }
+
+    public static List<String> read(String path){
+        try {
+            return readFile(path);
+        } catch (IOException | InvalidPathException e) {
+            return null;
+        }
+    }
+
+    private static List<String> readFile(String path) throws IOException, InvalidPathException {
+        return Files.readAllLines(Paths.get(path));
+    }
+}
---
+++ src/test/java/FileReaderTest.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class FileReaderTest {
+
+    @Test
+    public void readFileByPath() {
+        List<String> actual = FileReader.read("src/test/resources/reader/test.txt");
+
+        List<String> expected = Arrays.asList("test","text");
+
+        assertEquals(actual,expected);
+    }
+
+    @Test
+    public void readUndefinedFile(){
+        List<String> actual = FileReader.read("someUndefinedFile.txt");
+
+        assertNull(actual);
+    }
+
+    @Test
+    public void readIncorrectPathFile(){
+        List<String> actual = FileReader.read("!@#$%^&*(");
+
+        assertNull(actual);
+    }
+}
