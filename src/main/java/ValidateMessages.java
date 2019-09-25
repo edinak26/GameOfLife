49a0b4c1433f29375e71c00672811ca9ff0d78bb
Phoenix <edinak26@gmail.com>
2019-09-25 17:28:15 +0300

    Copy of ValidateMessages was deleted

--- src/main/java/ValidateMessages.java
+++
@@ -1,7 +0,0 @@
-public interface ValidateMessages {
-    String DEFAULT_INCORRECT_FILE_FORMAT = "Incorrect format of input.txt\n";
-    String INCORRECT_SIZE_LINE_FORMAT = "Incorrect size line format";
-    String INCORRECT_ITERATIONS_LINE_FORMAT = "Incorrect iterations line format";
-    String INCORRECT_FIELD_CHARACTERS_IN = "Incorrect field characters in line ";
-    String INCORRECT_FIELD_WIDTH_IN = "Incorrect field width in line ";//, all lines must have same width";
-}
